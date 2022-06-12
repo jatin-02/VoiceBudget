9e26a6c6d9d873da8e68d3831eb5f913bfec7bb4
Jatin Soni <jatinsoni1202@gmail.com>
2022-06-12 15:30:53 +0530

    Initialize repo

---
+++ src/components/Details/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  income: {
+    borderBottom: "10px solid green",
+    backgroundColor: "#fff",
+  },
+  expense: {
+    borderBottom: "10px solid red",
+    backgroundColor: "#fff",
+  },
+}));
